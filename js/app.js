ba58a9017f3ab6a029d8ee39bbb70bdc8f19a6d7
odomaiya <odomaiya@gmail.com>
2026-02-05 16:04:32 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const CSV_URL = "COLE_AQUI_SEU_LINK_CSV";
+const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR7j4_2qhc-W7EscYgFNEoWX-jEUsfS8xPSnOkEGj7uf1xSUFKkANQ8YQ57UUZsPytia7Vq6iShxHGy/pub?gid=1004684059&single=true&output=csv
+";
 
 let produtos = [];
 let carrinho = {};
