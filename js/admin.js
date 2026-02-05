c99cd95d405cd85774ad2914d0e19217ecd7a61d
odomaiya <odomaiya@gmail.com>
2026-02-05 15:23:38 -0300

    Delete js/admin.js

--- js/admin.js
+++
@@ -1,8 +0,0 @@
-const dados = JSON.parse(localStorage.getItem("ranking")) || {};
-const ul = document.getElementById("ranking");
-
-Object.entries(dados)
-  .sort((a,b)=>b[1]-a[1])
-  .forEach(i=>{
-    ul.innerHTML += <li>${i[0]} — ${i[1]} vendas</li>;
-  });
