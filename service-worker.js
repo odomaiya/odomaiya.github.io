e5bec658654e0cfeaa8d24778290385cb6e4f6f8
odomaiya <odomaiya@gmail.com>
2026-02-20 10:45:16 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,13 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', () => self.clients.claim());
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open("odomaia-v1").then(cache=>{
+return cache.addAll(["index.html","estilo.css","app.js"]);
+})
+);
+});
+
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then(res=>res||fetch(e.request))
+);
+});
