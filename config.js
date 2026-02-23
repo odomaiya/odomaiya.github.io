ce2ec6c5cbf25599d8664d5d364df9cf495e5e61
odomaiya <odomaiya@gmail.com>
2026-02-23 13:35:59 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const CONFIG = {
+  whatsapp: "5496048808",
+  loja: {
+    nome: "Odòmáiyà Artigos Religiosos",
+    endereco: "R. Sete de Agosto, 28 - Centro - Passo Fundo - RS",
+    instagram: "@odomaia_artigosreligiosos"
+  },
+  planilhaCSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR7j4_2qhc-W7EscYgFNEoWX-jEUsfS8xPSnOkEGj7uf1xSUFKkANQ8YQ57UUZsPytia7Vq6iShxHGy/pub?gid=1004684059&single=true&output=csv",
+  taxaEntrega: null
+};
