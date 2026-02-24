88eb96d35019240b1da2458d3600eed131078c01
odomaiya <odomaiya@gmail.com>
2026-02-24 14:18:16 -0300

    Create store.js

---
+++ js/store.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbzPHF-hrcCEbr20fbk8LaBxbPMHEXra9sw0l7xU8tCOzDZu2PUW899fLqnwap1aGJx0/exec",
+  WHATSAPP: "555496048808",
+  ENDERECO: "R. Sete de Agosto, 28 - Centro, Passo Fundo - RS, 99025-030",
+  LOJA: "Odòmàiyá Artigos Religiosos"
+};
