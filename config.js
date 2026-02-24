2869ac2081169eb4434b8d73282de5a3f8fc0b24
odomaiya <odomaiya@gmail.com>
2026-02-24 14:31:15 -0300

    Delete config.js

--- config.js
+++
@@ -1,7 +0,0 @@
-const CONFIG = {
-  planilhaCSV: "https://docs.google.com/spreadsheets/d/e/2PACX-1vR7j4_2qhc-W7EscYgFNEoWX-jEUsfS8xPSnOkEGj7uf1xSUFKkANQ8YQ57UUZsPytia7Vq6iShxHGy/pub?gid=1004684059&single=true&output=csv",
-  whatsapp: "555496048808",
-  loja: {
-    endereco: "R. Sete de Agosto, 28 - Centro - Passo Fundo - RS"
-  }
-};
