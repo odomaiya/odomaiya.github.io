b8ecfbc218bc66baf934e2d73b3aa983c576f595
odomaiya <odomaiya@gmail.com>
2026-02-24 15:38:44 -0300

    Delete js/config.js

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbzPHF-hrcCEbr20fbk8LaBxbPMHEXra9sw0l7xU8tCOzDZu2PUW899fLqnwap1aGJx0/exec";
-
-const WHATSAPP_NUMERO = "555496048808";
-
-const NOME_LOJA = "Odòmàiyá Artigos Religiosos";
-const ENDERECO_LOJA = "R. Sete de Agosto, 28 - Centro, Passo Fundo - RS, 99025-030";
