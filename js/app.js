ab9a7677fb6d53a5a170269b114e34422218f6f6
odomaiya <odomaiya@gmail.com>
2026-03-02 17:00:59 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,16 @@ if("serviceWorker" in navigator){
 navigator.serviceWorker.register("sw.js");
 }
 });
+document.getElementById("tipo").addEventListener("change",e=>{
+document.getElementById("endereco-box").style.display=
+e.target.value==="entrega"?"block":"none"
+})
+
+document.getElementById("cep").addEventListener("input",async e=>{
+if(e.target.value.length===8){
+const r=await fetch(`https://viacep.com.br/ws/${e.target.value}/json/`)
+const d=await r.json()
+rua.value=d.logradouro
+cidade.value=d.localidade
+}
+})
