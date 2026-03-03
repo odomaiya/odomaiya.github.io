e37d1fe3928f1a36d2dd1fccbe8c6305e4bcae9f
odomaiya <odomaiya@gmail.com>
2026-03-03 10:29:58 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,17 +1,8 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyNDOjR9YM5JBAU42gUcwGfyZPwSaVdP6T9o73vEf-IuwT3f7qqeOP8CCUZGxv_dANy/exec";
 
 const API = {
-
-async getProdutos(){
-const r = await fetch(`${API_URL}?action=getProdutos`);
-return await r.json();
-},
-
-async registrarVenda(data){
-await fetch(API_URL,{
-method:"POST",
-body:JSON.stringify(data)
-});
-}
-
+  async carregarProdutos(){
+    const res = await fetch(API_URL);
+    return await res.json();
+  }
 };
