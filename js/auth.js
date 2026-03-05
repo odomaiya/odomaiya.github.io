4104a66527cf398397e39cce5c930aca3b85d689
odomaiya <odomaiya@gmail.com>
2026-03-05 07:47:45 -0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,41 @@
+const ADMIN_USER="adm"
+const ADMIN_PASS="99861309"
+
+function login(){
+
+const user=document.getElementById("user").value
+const pass=document.getElementById("pass").value
+
+if(user===ADMIN_USER && pass===ADMIN_PASS){
+
+localStorage.setItem("adminAuth","true")
+
+window.location="admin.html"
+
+}else{
+
+alert("Login inválido")
+
+}
+
+}
+
+function verificarAuth(){
+
+const auth=localStorage.getItem("adminAuth")
+
+if(auth!=="true"){
+
+window.location="admin.html?login"
+
+}
+
+}
+
+function logout(){
+
+localStorage.removeItem("adminAuth")
+
+window.location="/"
+
+}
