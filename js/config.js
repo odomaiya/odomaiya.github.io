c30745e630ba9a84eff2b266c1eb9c6813786688
odomaiya <odomaiya@gmail.com>
2026-03-05 07:25:12 -0300

    Update and rename analytics.js to config.js

---
+++ js/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+
+API_URL:"https://script.google.com/macros/s/AKfycbyNDOjR9YM5JBAU42gUcwGfyZPwSaVdP6T9o73vEf-IuwT3f7qqeOP8CCUZGxv_dANy/exec",
+
+WHATSAPP:"555496048808",
+
+LOJA:{
+nome:"Odòmáiyà Artigos Religiosos",
+endereco:"R. Sete de Agosto, 28 - Centro, Passo Fundo - RS"
+}
+
+}
