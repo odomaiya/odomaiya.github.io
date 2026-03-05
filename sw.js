3259323aa0844a700887614cc592d55b8b076280
odomaiya <odomaiya@gmail.com>
2026-03-05 07:28:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,3 @@
-self.addEventListener("install", event => {
-  self.skipWaiting();
-});
+self.addEventListener("install",e=>self.skipWaiting());
 
-self.addEventListener("activate", event => {
-  event.waitUntil(clients.claim());
-});
+self.addEventListener("activate",e=>clients.claim());
