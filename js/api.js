b52ea8f27a5c4f88e4c6c82ab78361365e41a458
odomaiya <odomaiya@gmail.com>
2026-03-05 08:15:25 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,9 @@
 const API_URL="https://script.google.com/macros/s/AKfycbyNDOjR9YM5JBAU42gUcwGfyZPwSaVdP6T9o73vEf-IuwT3f7qqeOP8CCUZGxv_dANy/exec";
 
-async function carregarProdutos(){
+async function buscarProdutos(){
 
  const res=await fetch(API_URL);
+
  const data=await res.json();
 
  return data.map(p=>({
