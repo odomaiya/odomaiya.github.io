38c7e8fcd1ea7d05e9874f1cdc0c7cd6d1c6a64d
odomaiya <odomaiya@gmail.com>
2026-03-05 07:25:31 -0300

    Update and rename api.js to store.js

--- js/api.js
+++
@@ -1,8 +0,0 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbyNDOjR9YM5JBAU42gUcwGfyZPwSaVdP6T9o73vEf-IuwT3f7qqeOP8CCUZGxv_dANy/exec";
-
-const API = {
-  async carregarProdutos(){
-    const res = await fetch(API_URL);
-    return await res.json();
-  }
-};
