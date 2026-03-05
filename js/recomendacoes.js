de7f0138ec620a3f71169edfb6662d47489f4c15
odomaiya <odomaiya@gmail.com>
2026-03-05 08:15:55 -0300

    Update recomendacoes.js

--- js/recomendacoes.js
+++ js/recomendacoes.js
@@ -1,7 +1,44 @@
-function gerarRecomendacoes(produtos){
+let historico=JSON.parse(localStorage.getItem("historico"))||[];
 
-let embaralhado=[...produtos].sort(()=>0.5-Math.random())
+function registrarInteresse(nome){
 
-return embaralhado.slice(0,4)
+ historico.push(nome);
+
+ if(historico.length>50){
+  historico.shift();
+ }
+
+ localStorage.setItem("historico",JSON.stringify(historico));
+
+}
+
+function recomendar(produtos){
+
+ if(historico.length===0){
+
+  return produtos
+  .filter(p=>produtoDisponivel(p))
+  .slice(0,4);
+
+ }
+
+ let categorias={};
+
+ historico.forEach(nome=>{
+
+  let prod=window.listaProdutos.find(p=>p.nome===nome);
+
+  if(!prod) return;
+
+  categorias[prod.categoria]=(categorias[prod.categoria]||0)+1;
+
+ });
+
+ let categoriaFavorita=Object.keys(categorias).sort((a,b)=>categorias[b]-categorias[a])[0];
+
+ return produtos
+ .filter(p=>p.categoria===categoriaFavorita)
+ .filter(p=>produtoDisponivel(p))
+ .slice(0,4);
 
 }
