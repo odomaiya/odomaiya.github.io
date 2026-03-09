a8b42385da59c92a86550ffa0361ef3a36f5ed85
odomaiya <odomaiya@gmail.com>
2026-03-09 10:58:57 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,29 +1,15 @@
 const API_URL="https://script.google.com/macros/s/AKfycbyNDOjR9YM5JBAU42gUcwGfyZPwSaVdP6T9o73vEf-IuwT3f7qqeOP8CCUZGxv_dANy/exec"
 
-async function carregarProdutos(){
+ export async function carregarProdutos() {
 
- try{
+  const url = CONFIG.PRODUTOS_URL;
 
-  const res=await fetch(API_URL)
+  const res = await fetch(url);
+  const data = await res.json();
 
-  const dados=await res.json()
+  if (!Array.isArray(data)) {
+    return [];
+  }
 
-  if(Array.isArray(dados)) return dados
-
-  if(dados.data) return dados.data
-
-  if(dados.produtos) return dados.produtos
-
-  console.error("API retornou formato inválido",dados)
-
-  return []
-
- }catch(e){
-
-  console.error("Erro API",e)
-
-  return []
-
- }
-
+  return data;
 }
