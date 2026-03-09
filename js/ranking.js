14949a09eba349b781dac8ea97837abd71f446cf
odomaiya <odomaiya@gmail.com>
2026-03-09 14:22:00 -0300

    Update ranking.js

--- js/ranking.js
+++ js/ranking.js
@@ -1,19 +1,19 @@
+"use strict";
+
 function rankingMaisVendidos(produtos){
 
-let vendas=JSON.parse(localStorage.getItem("ranking"))||{}
+ let vendas=JSON.parse(localStorage.getItem("ranking"))||{}
 
-produtos.forEach(p=>{
+ produtos.forEach(p=>{
+ if(!vendas[p.id]) vendas[p.id]=0
+ })
 
-if(!vendas[p.id]) vendas[p.id]=0
+ let ordenado=[...produtos].sort((a,b)=>{
 
-})
+ return (vendas[b.id]||0)-(vendas[a.id]||0)
 
-let ordenado=produtos.sort((a,b)=>{
+ })
 
-return vendas[b.id]-vendas[a.id]
-
-})
-
-return ordenado.slice(0,4)
+ return ordenado.slice(0,4)
 
 }
