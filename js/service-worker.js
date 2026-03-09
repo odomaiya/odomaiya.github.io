9807ef456a773859d4f0f460d7ee1e703028f556
odomaiya <odomaiya@gmail.com>
2026-03-09 11:55:49 -0300

    Update service-worker.js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,5 +1,32 @@
-self.addEventListener("install", e=>{
- console.log("SW instalado")
+const CACHE_NAME="odomaiya-v1"
+
+const ASSETS=[
+
+"/",
+"/index.html",
+"/css/style.css",
+"/js/app.js"
+
+]
+
+self.addEventListener("install",event=>{
+
+ event.waitUntil(
+
+  caches.open(CACHE_NAME)
+  .then(cache=>cache.addAll(ASSETS))
+
+ )
+
 })
 
-self.addEventListener("fetch", e=>{})
+self.addEventListener("fetch",event=>{
+
+ event.respondWith(
+
+  caches.match(event.request)
+  .then(res=> res || fetch(event.request))
+
+ )
+
+})
