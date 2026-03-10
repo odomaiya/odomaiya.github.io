6bca84dc6affbe331c27ec7a5629cac43504dd26
odomaiya <odomaiya@gmail.com>
2026-03-10 16:36:44 -0300

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,15 +1,18 @@
-const API="https://script.google.com/macros/s/AKfycbzKW5BVJQBvZAdqV7CVfxgpqJEpp33l-sK-IYphC22AbLxBu04ML8D9l25fB5hktSty/exec"
+const api = "https://script.google.com/macros/s/AKfycbzKW5BVJQBvZAdqV7CVfxgpqJEpp33l-sK-IYphC22AbLxBu04ML8D9l25fB5hktSty/exec"
 
-async function api(action,data){
+async function apiRequest(action,data={}){
 
-const res=await fetch(API,{
+const res = await fetch(api,{
 method:"POST",
+headers:{
+"Content-Type":"application/json"
+},
 body:JSON.stringify({
 action,
-data
+...data
 })
 })
 
-return await res.json()
+return res.json()
 
 }
