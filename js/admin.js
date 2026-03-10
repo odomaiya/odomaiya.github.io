6c2fc48b01ca7c79ca74c9a02fa0e13cb08a53df
odomaiya <odomaiya@gmail.com>
2026-03-10 09:04:54 -0300

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -1,25 +1,32 @@
-const Admin={
-
- pedidos:[],
-
- dashboard(){
-
-  console.log("Admin iniciado")
-
- },
-
- vendasTotais(){
-
-  return Admin.pedidos.reduce((t,p)=>t+p.total,0)
-
- }
-
-}
-
 document.addEventListener("DOMContentLoaded",()=>{
 
- if(!document.body.classList.contains("admin"))return
+const list=document.getElementById("adminList")
 
- Admin.dashboard()
+if(!list)return
+
+let promo=0
+let stock=0
+
+STATE.produtos.forEach(p=>{
+
+if(p.promocao=="true") promo++
+
+if(p.estoque<10) stock++
+
+const div=document.createElement("div")
+
+div.innerHTML=`
+<strong>${p.nome}</strong>
+Preço: ${p.preco}
+Estoque: ${p.estoque}
+`
+
+list.appendChild(div)
+
+})
+
+document.getElementById("prodCount").textContent=STATE.produtos.length
+document.getElementById("promoCount").textContent=promo
+document.getElementById("stockCount").textContent=stock
 
 })
